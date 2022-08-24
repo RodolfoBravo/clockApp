63e8f24cba8dbec359ac52d53bf1c7d3abbd3177
RodolfoBravo <rodolfobravogarcia@gmail.com>
2022-08-23 23:27:49 -0500

    this is my first commit of clock app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breakLenght: 5,
+      sessionLenght:25
+    }
+  }
+  render() {
+    return (
+      <main className='container'>
+        <div className='row'>
+          <div className='col'>
+            <p id='break-label'>Break Length</p>
+            <div className='col d-flex flex-row'>
+            <button id='break-increment'><i className="fa-solid fa-arrow-down"></i></button>
+            <p id='break-length'>{this.state.breakLenght}</p>
+            <button id='break-decrement'><i className="fa-solid fa-arrow-up"></i></button>
+            </div>
+          </div>
+
+          <div className='col'>
+            <p id='session-label'>Session Length</p>
+            <div className='col d-flex flex-row'>
+            <button id='session-increment'><i className="fa-solid fa-arrow-down"></i></button>
+            <p id='session-length'>{this.state.sessionLenght}</p>
+            <button id='session-decrement'><i className="fa-solid fa-arrow-up"></i></button>
+            </div>
+          </div>
+
+        </div>
+      </main>
+    )
+  }
 }
+
 
 export default App;
